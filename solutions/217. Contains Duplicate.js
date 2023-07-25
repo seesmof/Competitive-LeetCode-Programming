f1d8153ca7f11496b736fc29bb291bf3c01038a9
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-25 22:05:33 +0300

    update '217. Contains Duplicate.js'

--- solutions/217. Contains Duplicate.js
+++ solutions/217. Contains Duplicate.js
@@ -14,3 +14,12 @@ var containsDuplicate = function (nums) {
   }
   return false;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const set = new Set(nums);
+  return set.size !== nums.length;
+};
