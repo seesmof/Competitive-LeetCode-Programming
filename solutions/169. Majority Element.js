96fe50521947615305718a4d7b029b4ae9b46ec8
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-25 23:42:35 +0300

    update '169. Majority Element.js'

--- solutions/169. Majority Element.js
+++ solutions/169. Majority Element.js
@@ -21,3 +21,18 @@ var majorityElement = function (nums) {
     }
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    obj[num[i]] = obj[nums[i]] + 1 || 1;
+    if (obj[num[i]] > nums.length / 2) {
+      return num[i];
+    }
+  }
+};
