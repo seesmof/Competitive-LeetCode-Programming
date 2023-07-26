4678e48f715b6c570c4c57ee72e6c1397b1cbdc7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-07-26 09:30:05 +0000

    🌿: Wednesday, July 26, 2023 at 12:30:05 PM Eastern European Summer Time

---
+++ solutions/26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  const result = [];
+
+  return result.length;
+};
+// @lc code=end
