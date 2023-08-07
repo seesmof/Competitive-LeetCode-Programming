05732a4fc02e8ad8c3e278eb92910bfda9d593ab
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-07 17:47:16 +0000

    🌿: Monday, August 7, 2023 at 8:47:16 PM Eastern European Summer Time

---
+++ solutions/21.merge-two-sorted-lists.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=21 lang=typescript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+
+};
+// @lc code=end
+
