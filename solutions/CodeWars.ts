8206f4ad732fa5cfd973e4c73a1ebc7f81e1b328
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-08 13:26:35 +0300

    update CodeWars.ts

--- solutions/CodeWars.ts
+++ solutions/CodeWars.ts
@@ -1,7 +1,20 @@
-function getMiddle(s) {
-  //Code goes here!
-  if (s.length % 2 === 0) {
-    return s[Math.floor(s.length / 2)] + s[Math.floor(s.length / 2) + 1];
+export function findMissingLetter(array: string[]): string {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetUpper = alphabet.toUpperCase();
+  // get an index of the first letter in the alphabet array
+  const index = alphabet.indexOf(array[0]);
+  // iterate through the given array and check if the current letter is in the alphabet
+  for (let i = 1; i <= array.length; i++) {
+    if (array[i] === array[i].toLowerCase()) {
+      if (array[i] !== alphabet[index]) {
+        return alphabet[index];
+      }
+    } else if (array[i] === array[i].toUpperCase()) {
+      if (array[i] !== alphabetUpper[index]) {
+        return alphabetUpper[index];
+      }
+    }
   }
-  return s[Math.floor(s.length / 2)];
+
+  return "";
 }
