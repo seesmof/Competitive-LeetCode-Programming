027c88fc5684d3d01f7d5bc22b958ef10cb73399
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-08 11:31:27 +0300

    update CodeWars.ts

--- solutions/CodeWars.ts
+++ solutions/CodeWars.ts
@@ -1,3 +1,19 @@
-export function createPhoneNumber(numbers: number[]): string {
-  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+import { MORSE_CODE } from "./preloaded";
+export function decodeMorse(morseCode: string): string {
+  let words = morseCode.split("   ");
+  const hasSpaces = /^\s*\s*$/g;
+  words = words.filter((word) => !hasSpaces.test(word));
+  const decodedWords: string[] = [];
+  for (let word of words) {
+    const letters = word.split(" ");
+    for (let i = 0; i < letters.length; i++) {
+      const letter = MORSE_CODE[letters[i]];
+      if (letter) {
+        letters[i] = letter;
+      }
+    }
+    word = letters.join("");
+    decodedWords.push(word);
+  }
+  return decodedWords.join(" ");
 }
