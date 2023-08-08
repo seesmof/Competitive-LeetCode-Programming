b4e37602e6cf21e195cb58351addfe59f5174522
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-08 12:51:43 +0300

    update CodeWars.ts

--- solutions/CodeWars.ts
+++ solutions/CodeWars.ts
@@ -1,12 +1,15 @@
-function isIsogram(str) {
-  //...
-  if (str.length === 0) return true;
+function getCount(str) {
+  let vowels = 0;
   for (let i = 0; i < str.length; i++) {
-    for (let j = i + 1; j < str.length; j++) {
-      if (str[i].toLowerCase() === str[j].toLowerCase()) {
-        return false;
-      }
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      vowels++;
     }
   }
-  return true;
+  return vowels;
 }
