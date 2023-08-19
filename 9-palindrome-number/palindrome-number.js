f585e2bc28670b366e7b4b818b5e6a1c87c805a7
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2023-08-19 12:48:08 +0300

    Time: 140 ms (84.11%) | Memory: 51.8 MB (12.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,9 @@
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  return x.toString() === x.toString().split("").reverse().join("");
+};
