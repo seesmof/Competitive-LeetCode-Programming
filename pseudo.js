0c9240da3c51286157abc6aaa0430d4f4c8cf544
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-19 11:26:29 +0000

    🌿: Saturday, August 19, 2023 at 2:26:29 PM Eastern European Summer Time

---
+++ pseudo.js
@@ -0,0 +1,5 @@
+const multiply = (a, b) => a * b;
+
+for (let i = 0; i < 100; i++) {
+  console.log(multiply(2, i));
+}
--- pseudo.ts
+++ pseudo.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const multiplyTwoNumbers = (a: number, b: number) => a * b;
+
+for (let i = 0; i < 10; i++) {
+  console.log(multiplyTwoNumbers(i, i));
+}
+
+let a, b, c;
+a = 2;
+b = 4;
+c = 6;
+console.log(a + b + c);
