d65577183bed1570a97ee0a5adafab3dca33c52b
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-12 15:09:27 +0300

    feat: create 217.contains-duplicate.py

---
+++ solutions/217.contains-duplicate.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        st = set(nums)
+        return len(st) != len(nums)
+# @lc code=end
