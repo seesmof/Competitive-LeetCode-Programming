51de8b4bc973484a045a035918474ea486a0e598
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-13 14:13:02 +0300

    update pseudo.py and 135.candy.py

--- solutions/135.candy.py
+++ solutions/135.candy.py
@@ -8,19 +8,18 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
         # okay what do we have in here? a list of numbers, and we need to run through them all to see the differences between each and if one is distinct then it deserves a buff in the amount of candies. lets try brute forcing
-        c = len(ratings)
-        # here we automatically complete the first
+        c = {}
         for i in range(len(ratings)):
             if i == 0:
                 if ratings[i] > ratings[i+1]:
-                    c += 1
+                    c[ratings[i]] = 1
             elif i == len(ratings)-1:
                 if ratings[i] > ratings[i-1]:
-                    c += 1
+                    c[ratings[i]] = c[ratings[i-1]] + 1
             else:
                 if ratings[i] > ratings[i+1]:
-                    c += 1
-        return c
+                    c[ratings[i]] = c[ratings[i+1]] + 1
+        return sum(c.values())
         # okay our brute force doesnt work so we need to come up with something else. so right now we're just checking if the next element is more than the current one, if it is then we add one to the candies.
         # tweaked it up a bit, accounted for the edge cases like the first and last elements, but still no luck when submitting
         # yeah i dont really know, my mental abilities end there, lets watch the solution and learn about greedy algorithms.
