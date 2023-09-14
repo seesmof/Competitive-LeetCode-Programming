cc38870bd1d931f128f0f1d9e6383b229144e8e1
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-14 12:15:49 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,11 +1,11 @@
+def countDifferentNumbers(n):
+    seen = set()
+    for i in range(1, n+1):
+        see = n % i
+        if see not in seen:
+            seen.add(see)
+    return len(seen)
+
+
 n = int(input())
-# print(f"{n//10} {n % 10}")
-print()
-seen = set()
-for i in range(1, n+1):
-    print(f"\n{i} and {n%i}\n")
-    if n % i not in seen:
-        seen.add(i)
-print()
-print(seen)
-print(len(seen))
+print(countDifferentNumbers(n))
