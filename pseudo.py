ae789852aecd996bd6a70bd6b4c05388ab17fda7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-14 16:42:22 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,26 +1,21 @@
-def countMinMillions(n, m, c):
-    lenC = len(c)
-    results = []
-    for i in range(lenC):
-        for j in range(i+1, lenC):
-            results.append((c[i]+c[j]) % m)
-    minResults = []
-    print(f"\n{results}\n")
-    for i in range(len(results)):
-        if results[i] == 0:
+MOD = 10 ** 9 + 7
+n = int(input())
+a = [int(x) for x in input().split()]
+
+
+def calculateNumberOfArrays(n, a):
+    for i in range(n):
+        try:
+            if 2 <= a[i] <= n - 1:
+                x = a[i]
+                a[i-1] += x
+                a[i+1] += x
+                a[i] = -x
+            else:
+                continue
+        except:
             continue
-        tmp = results[i]
-        print(f"\n{tmp}\n")
-        for j in range(i+1, len(results)):
-            tmp += results[j]
-            print(f"cur = {results[i]+results[j]}")
-        print()
-        minResults.append(tmp)
-    print(minResults)
-    return min(minResults)
+    return a
 
 
-n, m = input().split()
-n, m = int(n), int(m)
-c = [int(i) for i in input().split()]
-print(countMinMillions(n, m, c))
+print(calculateNumberOfArrays(n, a))
