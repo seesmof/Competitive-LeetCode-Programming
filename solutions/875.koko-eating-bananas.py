8fd4331dead19c974860548e2e0c7155530f9201
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-17 14:25:17 +0300

    delete 21 files

---
+++ solutions/875.koko-eating-bananas.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=875 lang=python3
+#
+# [875] Koko Eating Bananas
+#
+
+# @lc code=start
+class Solution:
+    def minEatingSpeed(self, piles, h):
+        
+# @lc code=end
+
