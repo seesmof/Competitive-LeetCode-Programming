45baafb418e049bc2d2a093888c82d5214674ee3
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-17 14:14:59 +0300

    update 1 file and delete 2 files

--- pseudo.py
+++ pseudo.py
@@ -1,7 +1,4 @@
-arr = []
+def searchMatrix(matrix, target):
+    rows, cols = len(matrix), len(matrix[0])
 
-matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
-for row in matrix:
-    for item in row:
-        arr.append(item)
-print(arr)
+    t, b = 0, rows - 1
