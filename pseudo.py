6519104156c92fa8340a942228476b35709b242c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-18 11:00:14 +0300

    feat: solve test problem

--- pseudo.py
+++ pseudo.py
@@ -1,39 +1,33 @@
-import random
+"""
+The array M[1: N] of positive integers is given, ordered in increasing order.
 
-names = [
-    "Isabel Tran",
-    "John Glover",
-    "Mollie Rodgers",
-    "Julia Page",
-    "Eula Green",
-    "Hester Copeland",
-    "Landon Castillo",
-]
-names = sorted(names)
+Find the first positive integer that is not represented by the sum of any elements of this array, the sum can consist of one term, but each element of the array can be included only once.
+"""
+
+m = [21, 28, 9, 91, 88, 100, 41, 66, 7, 20, 76, 38, 45, 22, 5, 1, 35]
+m = sorted(set(m))
 
 
-def search(arr, target):
-    low, high = 0, len(arr) - 1
+def checkForSum(arr: [int], target: int) -> bool:
+    min, max = 0, len(arr) - 1
 
-    while low <= high:
-        mid = (low + high) // 2
-        guess = arr[mid]
-
-        if guess == target:
-            return mid
-        if guess > target:
-            high = mid - 1
+    while min <= max:
+        sum = arr[min] + arr[max]
+        if sum == target:
+            return True
+        if sum > target:
+            max -= 1
         else:
-            low = mid + 1
+            min += 1
+    return False
 
+
+def findFirstEl(arr: [int]) -> int:
+    for i in range(len(arr)):
+        if not checkForSum(arr, arr[i]):
+            return i
     return -1
 
 
-guess = random.choice(names)
-print(f"\nSearching for \"{guess}\" in {', '.join([f'{name}' for name in names])}\n")
-result = search(names, guess)
-if result != -1:
-    print(f"Found at {result}")
-else:
-    print(f"Not found :(")
-print()
+ans = findFirstEl(m)
+print(f"\n{ans}\n")
