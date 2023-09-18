9ddd85caf8d5d16028669974c801a613a342924c
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-18 11:30:34 +0300

    test:

--- pseudo.py
+++ pseudo.py
@@ -41,35 +41,24 @@ arrs = [
 
 
 def checkForSum(arr: [int], target: int) -> bool:
-    # use a set to store the possible sums
-    sums = set()
-    # loop through the array and add each element to the existing sums
-    for x in arr:
-        # if x is equal to or greater than the target, no need to continue
-        if x >= target:
-            break
-        # add x to the set
-        sums.add(x)
-        # loop through the existing sums and add x to them
-        for y in list(sums):
-            # if the new sum is equal to or greater than the target, no need to continue
-            if x + y >= target:
-                break
-            # add the new sum to the set
-            sums.add(x + y)
-    # check if the target is in the set
-    return target in sums
+    min, max = 0, len(arr) - 1
+
+    while min <= max:
+        sum = arr[min] + arr[max]
+        if sum == target:
+            return True
+        if sum > target:
+            max -= 1
+        else:
+            min += 1
+    return False
 
 
 def findFirstEl(arr: [int]) -> int:
-    # start from 1 and increment until we find a number that is not in the array or a sum of its elements
-    i = 1
-    while True:
-        # if i is not in the array and not a sum of its elements, return i
-        if i not in arr and not checkForSum(arr, i):
+    for i in range(len(arr)):
+        if not checkForSum(arr, arr[i]):
             return i
-        # otherwise, increment i
-        i += 1
+    return -1
 
 
 def tests():
