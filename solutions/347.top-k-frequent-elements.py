a3e4557e2b0d431bedc1f3b32ccc75032d1003e2
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-19 09:40:47 +0300

    feat: 347 -

--- solutions/347.top-k-frequent-elements.py
+++ solutions/347.top-k-frequent-elements.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 #
 # @lc app=leetcode id=347 lang=python3
 #
@@ -10,25 +8,16 @@ from collections import defaultdict
 
 
 class Solution:
-    def topKFrequent(self, nums, k):
-        ocs = defaultdict(list)
-        ans = []
-        ori = list()
+    def topKFrequent(self, nums: [int], k: int):
+        print(sorted(nums))
+        arr = list(set(nums))
+        res = []
+        print(arr)
 
-        for n in nums:
-            ocs[n].append(n)
+        for i in range(k):
+            res.append(arr[i % len(arr)])
+        res = sorted(res)
+        return res
 
-            if n not in ori:
-                ori.append(n)
 
-        count = 0
-        while k > 0:
-            ans.append(ori[count])
-            k -= 1
-            if count < len(ori):
-                count += 1
-            else:
-                count = 0
-
-        return ans
 # @lc code=end
