466b032c81746fa17d0f661ef8f5549be6cb2216
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-19 17:22:00 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,3 +1,5 @@
+import random
+
 arr = [4, 9, 3, 6, 2]
 
 
@@ -12,12 +14,24 @@ def selectionSort(arr: [int]) -> [int]:
     return res
 
 
+# recursively sum all elements
 def recursiveSum(arr: [int]) -> int:
     return 0 if not arr else arr[0] + recursiveSum(arr[1:])
 
 
+# recursively count number of elements
 def recursiveCountItems(arr: [int]) -> int:
     return 0 if not arr else 1 + recursiveCountItems(arr[1:])
+
+
+# recursively find the largest element
+def recursiveFindMax(arr: [int]) -> int:
+    n = len(arr)
+
+
+# recursively rewrite binary search
+def recursiveBinarySearch(arr: [int], target: int) -> int:
+    pass
 
 
 print(f"\nOriginla array: {arr}\n")
@@ -27,4 +41,17 @@ res = recursiveSum(arr)
 print(f"Sum of all elements: {res}")
 res = recursiveCountItems(arr)
 print(f"Length: {res}")
-print()
+res = recursiveFindMax(arr)
+print(f"Max element: {res}")
+res = recursiveBinarySearch(arr, random.choice(arr))
+print(f"Found at index: {res}")
+print("\n")
+
+
+def recursiveFacotrial(n):
+    return 1 if n == 1 else n * recursiveFacotrial(n - 1)
+
+
+x = random.choice(range(12))
+res = recursiveFacotrial(x)
+print(f"{x}! = {res}")
