ffa06ada7a22de46ddb5ef298247ca2276f63e90
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-19 09:27:44 +0300

    feat: 33 +

--- solutions/33.search-in-rotated-sorted-array.py
+++ solutions/33.search-in-rotated-sorted-array.py
@@ -8,18 +8,9 @@
 # @lc code=start
 class Solution:
     def search(self, nums: [int], target: int) -> int:
-        min, max = 0, len(nums) - 1
-
-        while min <= max:
-            mid = (min + max) // 2
-            guess = nums[mid]
-
-            if guess == target:
-                return mid
-            if guess > target:
-                max = mid - 1
-            else:
-                min = mid + 1
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
         return -1
 
 
