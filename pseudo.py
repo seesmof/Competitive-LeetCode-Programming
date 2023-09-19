d65dc2fa8818e9d6fe28e6f6757646ed02c4fe5a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-19 13:23:23 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,14 +1,9 @@
-def selectionSort(arr):
-    n = len(arr)
-    for i in range(n):
-        smallest = i
-        for j in range(i + 1, n):
-            if arr[j] < arr[smallest]:
-                smallest = j
-        arr[i], arr[smallest] = arr[smallest], arr[i]
+def countDown(n):
+    print(n)
+    if n <= 1:
+        return
+    else:
+        countDown(n - 1)
 
 
-arr = [31, 22, 15, 18, 26, 51, 92]
-print(arr)
-selectionSort(arr)
-print(arr)
+countDown(3)
