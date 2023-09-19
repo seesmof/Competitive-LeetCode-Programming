dbcfab061c50eaa630b87b3540f7dde8b9d96f23
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-19 12:51:19 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,1 +1,18 @@
-l, r = 1, max(piles)
+artists = {
+    "radiohead": 156,
+    "the black keys": 35,
+    "the strokes": 61,
+    "red hot chilli pepper": 231,
+    "nirvana": 176,
+}
+res = []
+
+while artists:
+    maxValue = max(artists.values())
+    res.append(maxValue)
+
+    maxKey = [key for key, value in artists.items() if value == maxValue][0]
+
+    del artists[maxKey]
+
+print(res)
