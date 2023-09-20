4d34e8b805234d59783cd6a1aefec51611032c9a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-20 19:24:25 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,78 +1,15 @@
-def quickSort(arr):
+def quickSort(arr: [int]):
     n = len(arr)
     if n < 2:
         return arr
 
-    pivot = arr[n // 2]
-    less = [el for el in arr if el < pivot]
-    equal = [el for el in arr if el == pivot]
-    more = [el for el in arr if el > pivot]
+    p = arr[n // 2]
+    less = [x for x in arr if x < p]
+    equal = [x for x in arr if x == p]
+    more = [x for x in arr if x > p]
 
     return quickSort(less) + equal + quickSort(more)
 
 
-arr = [
-    32,
-    15,
-    7,
-    90,
-    42,
-    18,
-    63,
-    51,
-    24,
-    8,
-    60,
-    82,
-    11,
-    37,
-    45,
-    5,
-    74,
-    28,
-    95,
-    69,
-    2,
-    56,
-    79,
-    23,
-    61,
-    17,
-    3,
-    88,
-    70,
-    48,
-    36,
-    1,
-    55,
-    9,
-    71,
-    33,
-    91,
-    14,
-    67,
-    29,
-    77,
-    6,
-    47,
-    13,
-    85,
-    19,
-    68,
-    41,
-    21,
-    86,
-    46,
-    25,
-    38,
-    4,
-    72,
-    64,
-    30,
-    57,
-    22,
-    31,
-]
-
-res = quickSort(arr)
-print(res)
+arr = [31, 52, 21, 73, 62, 17, 42, 83]
+print(quickSort(arr))
