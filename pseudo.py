a3b82d76d61d8347d4dd1bab8bfb4e22c5336f9a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-23 21:01:56 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -1,6 +1,9 @@
 def findMissingNumber(arr):
     arr = sorted(arr)
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         if arr[i + 1] != arr[i] + 1:
             return arr[i] + 1
-    return -1
+    return arr[-1] + 1
+
+
+print(findMissingNumber([1, 2, 4, 6, 3, 7, 8]))
