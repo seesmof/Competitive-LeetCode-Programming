3117c4e70d1bbafb653826073d39ce3ee907f490
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-24 18:29:53 +0300

    update pseudo.py

--- pseudo.py
+++ pseudo.py
@@ -2,25 +2,25 @@ class Heap:
     def __init__(self):
         self.heap = []
 
-    def _siftUp(self, i):
-        while (i - 1) // 2 >= 0:
-            if self.heap[(i - 1) // 2] < self.heap[i]:
-                self._swap((i - 1) // 2, i)
-            i = (i - 1) // 2
+    def _siftUp(self, index):
+        while (index - 1) // 2 >= 0:
+            if self.heap[(index - 1) // 2] < self.heap[index]:
+                self._swap((index - 1) // 2, index)
+            index = (index - 1) // 2
 
-    def _siftDown(self, i):
-        while 2 * i + 1 < len(self.heap):
-            max_child_index = self._getMaxChild(i)
-            if self.heap[max_child_index] > self.heap[i]:
-                self._swap(max_child_index, i)
-            i = max_child_index
+    def _siftDown(self, index):
+        while 2 * index + 1 < len(self.heap):
+            max_child_index = self._getMaxChild(index)
+            if self.heap[max_child_index] > self.heap[index]:
+                self._swap(max_child_index, index)
+            index = max_child_index
 
-    def _getMaxChild(self, i):
-        if 2 * i + 2 > len(self.heap) - 1:
-            return 2 * i + 1
-        if self.heap[2 * i + 1] > self.heap[2 * i + 2]:
-            return 2 * i + 1
-        return 2 * i + 2
+    def _getMaxChild(self, index):
+        if 2 * index + 2 > len(self.heap) - 1:
+            return 2 * index + 1
+        if self.heap[2 * index + 1] > self.heap[2 * index + 2]:
+            return 2 * index + 1
+        return 2 * index + 2
 
     def _swap(self, i, j):
         self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
@@ -50,4 +50,27 @@ class Heap:
 
     def display(self):
         for item in self.heap:
-            print(item)
+            print(item, end=" ")
+        print()
+
+
+heap = Heap()
+
+# Insert numbers into the heap
+for i in [2, 7, 1, 3, 5, 12, 8, 4, 6, 9, 11, 10]:
+    heap.insert(i)
+
+# Display the heap
+heap.display()
+
+# Delete the max item from the heap and display the heap
+print("Deleted:", heap.delete())
+heap.display()
+
+# Sort all items in the heap and display them
+sorted_items = heap.sort()
+print("Sorted items:", sorted_items)
+
+# Build heap from a list and display it
+heap.buildHeap([7, 12, 5, 6, 13, 9, 10, 15, 11, 8])
+heap.display()
