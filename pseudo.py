153f57f60117b8dab4be6d2db32970aaa53e1240
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-24 11:31:36 +0300

    update pseudo.py

---
+++ pseudo.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def prepand(self, data):
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            newNode = Node(data)
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+
+    def appendAfterNode(self, nodeData, data):
+        currentNode = self.head
+        while currentNode is not None:
+            if currentNode.data == nodeData:
+                newNode = Node(data)
+                newNode.prev = currentNode
+                newNode.next = currentNode.next
+                if currentNode.next is not None:
+                    currentNode.next.prev = newNode
+                currentNode.next = newNode
+                return
+            currentNode = currentNode.next
+
+    def search(self, data) -> bool:
+        currentNode = self.head
+        while currentNode is not None:
+            if currentNode.data == data:
+                return True
+            currentNode = currentNode.next
+        return False
+
+    def delete(self, data):
+        currentNode = self.head
+        while currentNode is not None:
+            if currentNode.data == data:
+                if currentNode.prev is not None:
+                    currentNode.prev.next = currentNode.next
+                if currentNode.next is not None:
+                    currentNode.next.prev = currentNode.prev
+                if currentNode == self.head:
+                    self.head = currentNode.next
+                return
+            currentNode = currentNode.next
+
+    def display(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.data, end=" -> ")
+            currentNode = currentNode.next
+        print("None")
+
+    def displayFromTail(self):
+        currentNode = self.head
+        while currentNode is not None:
+            currentNode = currentNode.next
+        print("None")
+
+
+LL = DoublyLinkedList()
+LL.prepand(21)
+LL.prepand(52)
+LL.prepand(33)
+LL.prepand(12)
+LL.prepand(26)
+LL.displayFromTail()
