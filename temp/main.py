f8bc8bfee62eff3ae8159371793c446f0f5f4cdd
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-24 11:44:37 +0300

    update main.py

--- temp/main.py
+++ temp/main.py
@@ -61,38 +61,20 @@ class DoublyLinkedList:
         return False
 
     def delete(self, data) -> bool:
-        # Start at the head of the linked list
         currentNode = self.head
-
-        # Iterate through the list
         while currentNode is not None:
-            # When the node with the data to be deleted is found
             if currentNode.data == data:
-                # If it is not the first node
                 if currentNode.prev is not None:
-                    # Update the next pointer of the previous node to skip the current node
                     currentNode.prev.next = currentNode.next
-
-                # If it is not the last node
                 if currentNode.next is not None:
-                    # Update the previous pointer of the next node to skip the current node
                     currentNode.next.prev = currentNode.prev
-
-                # If it is the head of the list
                 if currentNode == self.head:
-                    # Update the head to be the next node
                     self.head = currentNode.next
-
-                # Return True showing that the node was successfully deleted
                 return True
-
-            # Move to the next node
             currentNode = currentNode.next
-
-        # Return False if the node was not found in the list
         return False
 
-    def display(self):
+    def displayFromHead(self):
         currentNode = self.head
         while currentNode is not None:
             print(currentNode.data, end=" -> ")
@@ -118,4 +100,4 @@ LinkedList.append(52)
 LinkedList.append(33)
 LinkedList.append(12)
 LinkedList.append(26)
-LinkedList.display()
+LinkedList.displayFromHead()
