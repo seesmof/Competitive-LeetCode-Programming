9d2f53bbec2bca4d33588d510ea0fbc2189d8c0f
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-24 16:19:19 +0300

    update pseudo.py and test.py

--- temp/test.py
+++ temp/test.py
@@ -17,15 +17,3 @@ for employee in data["employees"]:
 
 for disease, count in sorted(disease_count.items(), key=lambda x: x[1], reverse=True):
     print(f"{disease}: {count}")
-
-"""
-Using range(len(arr)) in Python loops is not considered Pythonic for several reasons:
-
-1. It is less readable: When you use range(len(arr)) it is not immediately clear that you're iterating over the elements of the array.
-
-2. It is slower: Calling len(arr) is an O(1) operation in Python, but creating the list of indices up to len(arr) is an O(n) operation. For large lists, this is a significant overhead.
-
-One viable alternative to using range(len(arr)) is to use enumerate(arr), which returns each element along with its index. This is typically what you want if you're using range(len(arr)).
-
-Another alternative, if you don't need the index, is to directly use the list as your iterator (i.e., for element in arr). This is more Pythonic and faster.
-"""
