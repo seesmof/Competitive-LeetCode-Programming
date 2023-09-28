39ec9f038e1b1b4da14494aeb313dec72b9caf61
Oleh Onyshchenko <seesmwork@gmail.com>
2023-09-28 21:18:53 +0300

    feat: solve 9

--- archive/SP_lab1/9.py
+++ archive/SP_lab1/9.py
@@ -1,3 +1,20 @@
 """
-Дано послідовності X та Y. З'ясувати чи можна отримати послідовності X шляхом викреслювання елементів з послідовності Y.
+Задані Q та Y – дві послідовності. Чи можна отримати послідовність Q шляхом викреслення елементів з Y?
 """
+
+
+def solve(Q: [int], Y: [int]):
+    Q, Y = sorted(Q), sorted(Y)
+    return set(Q).issubset(Y)
+
+
+def tests():
+    assert solve([1, 2, 3], [1, 2, 3, 4, 5]) == True, "Test 1 failed"
+    assert solve([1, 2, 3], [1, 2]) == False, "Test 2 failed"
+    assert solve([], [1, 2, 3]) == True, "Test 3 failed"
+    assert solve([1, 2, 3], []) == False, "Test 4 failed"
+    assert solve([1, 2, 3], [3, 2, 1]) == True, "Test 5 failed"
+    print("All tests passed")
+
+
+tests()
