777bbb96262793f3a442e649138ff33b52ee2eb4
Oleh Onyshchenko <seesmwork@gmail.com>
2023-10-07 15:16:26 +0300

    feat: examples yep

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import http.server
+import socketserver
 
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
